82424e8acf533355d30e0ef31fd5934b95076f88
M.Adam <piratte65@gmail.com>
2014-12-10 18:30:53 +0100

    adding 4th DU, state: unstarted

---
+++ du4/CodEx.java
@@ -0,0 +1,31 @@
+/* Pouzijte implicitni nepojmenovany balicek, tj. nepouzijte "package" */
+
+import java.io.*;
+
+/** DU 4. !!! Nemente jmeno tridy !!!
+  * 
+  * @author !!! DOPLN SVE JMENO !!!
+  */
+public class CodEx {
+
+  public static void main(String[] argv) {
+    
+    /* VZOR: kod, ktery opisuje data ze standardniho vstupu na standardni vystup
+     * a konci pokud narazi na konec vstupu.
+     *
+     *
+     *  try {
+     *    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+     *    int c;
+     *    while ((c = input.read()) != -1) {
+     *      System.out.print((char) c);
+     *    }
+     *  } catch (IOException ex) {
+     *    System.out.println("Nastala IOException");
+     *  }
+     *
+     */
+
+  }
+}
+
