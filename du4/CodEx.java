7ef36443f83e97cad38f82225e94dacb14aaf9c0
M.Adam <piratte65@gmail.com>
2014-12-16 11:03:33 +0100

    bugfixes, status: working 75%

--- du4/CodEx.java
+++ du4/CodEx.java
@@ -8,6 +8,7 @@ import java.util.*;
   * @author !!! M.Adam !!!
   */
 public class CodEx {
+	static final boolean deb = false;
 
 	static String[] parse(String str) {
 		String[] strs = str.split("(?<=[^\\.a-zA-Z\\d])|(?=[^\\.a-zA-Z\\d])");
@@ -22,14 +23,32 @@ public class CodEx {
 		}
 		int i = 0;
 		String[] out = new String [parts.size()];
+		boolean ecko = false;
+		boolean znam = false;
 		for (String s : parts) {
-			out[i++] = s;
+			//debug("s: " + s);
+			if (s.matches("[0-9]*e")) 
+				ecko = true;
+			if (ecko && s.equals("-")){
+				znam = true;
+				out[i-1] += s;
+				ecko = false;
+			} else if (znam){
+				out[i-1] += s;
+				znam = false;
+			} else
+				out[i++] = s;
+			//debug("out: " + out[i-1]);
 		}
+
+		// v i je nyni opravdova velikost pole
+		out = Arrays.copyOfRange(out,0,i);
 		return out;
 	}
 
 		static void debug(String slovo) {
-		System.out.println("[DEBUG]: " + slovo);
+		if (deb)
+			System.out.println("[DEBUG]: " + slovo);
 	}
 
 	static String pridej(String kam, String co) {
@@ -42,8 +61,24 @@ public class CodEx {
 
 	static String zpracujVyraz(String vyraz) {
 		// workaround pro string zacinajici mezerou
-		if (Character.isWhitespace(vyraz.charAt(0)));
+		if (Character.isWhitespace(vyraz.charAt(0)))
 			vyraz = vyraz.substring(1);
+
+		// priblizovani minusu k cislu u zapornych cisel
+		boolean bylOp = true;
+		char[] chvyraz = vyraz.toCharArray();
+		for (int i = 0; i<chvyraz.length; ++i) {
+			// debug(bylOp + " char: " + chvyraz[i]);
+			if ((!bylOp) && (chvyraz[i] == '+' ||chvyraz[i] == '-' ||chvyraz[i] == '*' ||chvyraz[i] == '/' ))
+				bylOp = true;
+			else if (bylOp && chvyraz[i] == '-' && Character.isWhitespace(chvyraz[i+1])) {
+				chvyraz[i] = ' ';
+				chvyraz[++i] = '-';
+			} else if ( !Character.isWhitespace(chvyraz[i]))
+				bylOp = false;
+		}
+		vyraz = new String(chvyraz);
+		debug(vyraz);
 
 		String[] split = vyraz.split("\\s+");
 		String out = "";
@@ -52,16 +87,20 @@ public class CodEx {
 		double d = 0.0;
 		Queue<Character> stack = new Queue<Character>();
 		for (String atom : split) {
+			// debug("atom: " + atom);
 			try {
 				d = Double.parseDouble(atom);
 				// vime ze na vstupu je cislo, to staci napsat na vystup
 				out = pridej(out, atom);
 			} catch (NumberFormatException e) {
+				// debug("neni cislo");
 				// mame na vstupu operator
 				if (atom.length() > 1)
 					throw new RuntimeException("dvojmistnej operator");
 
 				// mame na vstupu validni operato
+				if (atom.length() == 0)
+					continue;
 				op = atom.charAt(0);
 				switch (op) {
 					// operatory s nejnizsi prioritou
@@ -69,11 +108,12 @@ public class CodEx {
 					case '-':
 						// pokud je na vrcholu operator s vyssi prioritou, vypiseme
 						try {
-							while (stack.showLast() == '/' || stack.showLast() == '*')
+							while (stack.showLast() == '/' || stack.showLast() == '*' || stack.showLast() == '+' || stack.showLast() == '-')
 								out = pridej(out, stack.popLast());
-						} catch (RuntimeException ee) {} finally {
+						} catch (RuntimeException ee) {} 
+						  finally {
 							stack.put(op);
-						}
+						} 
 						break;
 
 					// operatory s vyssi prioritou
@@ -163,113 +203,114 @@ public class CodEx {
 	static HashMap<String,Funkce> funMap = new HashMap<String,Funkce>();
 	static HashMap<String,Double> varMap = new HashMap<String,Double>();
 
-	public static void main(String[] argv) {
-		final boolean test = false;
-		
+	public static void main(String[] argv) {		
 		String line;
 		String[] lineArr;
 		final double lastDefVal = 0.0;
 
-		// =============== ZACATEK TESTU ============================
-		if (test){
-		line = "DEF foo(a,b,c) 2*a + 2*b -c";
-		lineArr = parse(line);
-		line = line.replaceAll("x", "" + 123);
-		System.out.println(line);
-		Funkce blah = new Funkce(lineArr);
-		String[] args = parse("30 last 20");
-		line = blah.dosad(args);
-		varMap.put("last",1.5);
-		// dosazovani hodnot za promenne
-				for (String s: varMap.keySet()) {
-					debug("tady: " + s);
-					line = line.replaceAll(" " + s + " " , " " + varMap.get(s) + " ");
-				}
-		debug(line);
-		}
-
-		// =============== KONEC TESTU ============================
-		if (!test) {
-
+		// debug(vyhodnot(zpracujVyraz("-1  - 2"))+"");
+		
 		varMap.put("last",lastDefVal);
 		double val = lastDefVal;
 		String dosazujeme = "";
 		try(BufferedReader input = new BufferedReader(new InputStreamReader(System.in))){
 			while ((line = input.readLine()) != null) {
+				try {	
+					// osetreni prazdne radky
+					if (line.trim().length() == 0)
+						continue;
 
-				// osetreni prazdne radky
-				if (line.trim().length() == 0)
-					continue;
+					lineArr = parse(line);
 
-				lineArr = parse(line);
+					if (lineArr[0].equals("DEF")) {
+						// definijeme si funkci
+						// TODO: dodelat ERROR v definici funkce
+						// je potreba? nestacilo by pri kazdem volani spatne definovane fce hodit ERROR?
+						if (funMap.containsKey(lineArr[1]))
+							throw new RuntimeException("Function already defined");
+						funMap.put(lineArr[1], new Funkce(lineArr));
+						varMap.put("last",lastDefVal);
+						continue;
+					} else if (funMap.containsKey(lineArr[0])) {
+						// nasli jsme identifikator ve funkcich, nyni misto funkce dosadime jeji definici
 
-				if (lineArr[0].equals("DEF")) {
-					// definijeme si funkci
-					// TODO: dodelat ERROR v definici funkce
-					// je potreba? nestacilo by pri kazdem volani spatne definovane fce hodit ERROR?
-					funMap.put(lineArr[1], new Funkce(lineArr));
-					varMap.put("last",lastDefVal);
-					continue;
-				} else if (funMap.containsKey(lineArr[0])) {
-					// nasli jsme identifikator ve funkcich, nyni misto funkce dosadime jeji definici
+						// nejdriv ozkousime, jestli fci volame se spravnym poctem parametru
+						int carek = 0;
+						for (String s : lineArr) {
+							if (s.equals(","))
+								++carek;
+						}
+						// vytahneme to, co se dosazuje za parametry
+						int parNum = funMap.get(lineArr[0]).getNumOfArgs();
+						if (parNum == 0)
+							line = funMap.get(lineArr[0]).getDef();
+						else /* mame vic jak 0 parametru */{
+							if (carek+1 != parNum)
+								throw new RuntimeException("Wrong parameters count");
+							String[] params = new String[parNum];
+							// inicializuji pole 
+							
+							int i = 0;
+							params[i] = "";
+							int ind = 2;
+							while(!lineArr[ind].equals(")")){
+								if(lineArr[ind].equals(",")) {
+									params[++i] = "";
+								}
+								else
+									params[i] += " " + lineArr[ind];
+								++ind;
+							}
 
-					// vytahneme to, co se dosazuje za parametry
-					int parNum = funMap.get(lineArr[0]).getNumOfArgs();
-					String[] params = new String[parNum];
-					int i = 2;
-					
-					for (int j=0; j<parNum; ++j) {
-						params[j] = lineArr[i];
-						i += 2;
+							// zavolame metodu na funkci, ktera dosadi za deklarovane argumenty konkretni parametry a vrati vyraz,
+							// ktery odpovida zavolani funkce
+							
+							line = funMap.get(lineArr[0]).dosad(params);
+						}
+					} else if (lineArr.length == 1) {
+						if (!varMap.containsKey(lineArr[0]))
+							varMap.put(lineArr[0], 0.0);
+						line = " " + line + " ";
+					} else if (lineArr[1].equals("=")) {
+						dosazujeme = lineArr[0];
+						line = sloz(lineArr,2);
+					} else {
+						line = sloz(lineArr,0);
 					}
-					
-					// zavolame metodu na funkci, ktera dosadi za deklarovane argumenty konkretni parametry a vrati vyraz,
-					// ktery odpovida zavolani funkce
-					line = funMap.get(lineArr[0]).dosad(params);
-				} else if (lineArr.length == 1) {
-					if (!varMap.containsKey(lineArr[0]))
-						varMap.put(lineArr[0], 0.0);
-					line = " " + line + " ";
-				} else if (lineArr[1].equals("=")) {
-					dosazujeme = lineArr[0];
-					line = sloz(lineArr,2);
-				} else {
-					line = sloz(lineArr,0);
-				}
-				line += " ";
-				// dosazovani hodnot za promenne
-				//debug("line: " + line);
-				for (String s: varMap.keySet()) {
-					//debug("key: " + s + " val: " + varMap.get(s));
-					line = line.replaceAll(" " + s + " " , " " + varMap.get(s) + " ");
-				}
+					line += " ";
+					// dosazovani hodnot za promenne
+					//debug("line: " + line);
+					for (String s: varMap.keySet()) {
+						//debug("key: " + s + " val: " + varMap.get(s));
+						line = line.replaceAll(" " + s + " " , " " + varMap.get(s) + " ");
+					}
 
-				// debug("pred vypoctem: " + line);
-				// nyni mame vyraz jako v minulem ukolu -> pouziju jeho reseni
-				try {				
-						// prevedu na postfix
-						line = zpracujVyraz(line);
-						//debug("po zpracovani: " + line);
-						// vyhodnotim a vypisu hodnotu
-						val = vyhodnot(line);
-						System.out.printf("%.5f\n", val);
+					debug("pred vypoctem: " + line);
+					// nyni mame vyraz jako v minulem ukolu -> pouziju jeho reseni
+							
+					// prevedu na postfix
+					line = zpracujVyraz(line);
+					debug("po zpracovani: " + line);
+					// vyhodnotim a vypisu hodnotu
+					val = vyhodnot(line);
+					System.out.printf("%.5f\n", val);
 						
-					} catch (RuntimeException e) {
-						//System.err.println("main error: " + e);
+				} catch (RuntimeException e) {
+						//debug("main error: " + e);
 						System.out.println(ERROR);
 						val = lastDefVal;
-					} 
-					varMap.put("last", val);
-					// debug("key: last val: " + varMap.get("last"));
-					if (!dosazujeme.isEmpty())
-						varMap.put(dosazujeme, val);
-					dosazujeme = "";
+				} 
+				varMap.put("last", val);
+				// debug("key: last val: " + varMap.get("last"));
+				if (!dosazujeme.isEmpty())
+					varMap.put(dosazujeme, val);
+				dosazujeme = "";
 
 			}
 		} catch(IOException ioex) {
 
 		}
-		}
+		
 	}
 
 	/* |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -304,25 +345,37 @@ public class CodEx {
 			for (int k = i; k< definice.length ; ++k) {
 				def = pridej(def,definice[k]);
 			}
-
+			for (int j=0; j<argc; ++j) {
+				def = def.replaceAll(""+argv[j], "__param__" + j);
+			}
+			if (argc == 0)
+				def = def.substring(2);
 		}
 
 		public String dosad(String[] args) {
 			// debug(argc + " " + args.length);
-			if (args.length != argc)
-				throw new RuntimeException("Wrong number of params");
 
+			for (String in : args) {
+				for (char ch : argv) {
+					if (in.equals(""+ch))
+						ch = ch;
+				}
+			}
 			String out = def;
 
 			for (int i=0; i<args.length ; ++i) {
 				// debug(argv[i] + args[i] + i);
-				out = out.replaceAll(""+argv[i], args[i]);
+				out = out.replaceAll("__param__" + i, args[i]);
 			}
 			return out;
 		}
 
 		public int getNumOfArgs(){
 			return argc;
+		}
+
+		public String getDef() {
+			return def;
 		}
 	}
 	/* |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
